ff7a657a064d8cfcc2655c1991fad9fcd521ff0b
VigneshRav <vignesh212000@gmail.com>
2025-05-23 13:33:22 +0530

    Update Recipe Completed

--- Controllers/recipeController.js
+++ Controllers/recipeController.js
@@ -1,6 +1,6 @@
 import Recipes from "../Models/recipeModel.js";
 
-//Create Recipe (Post Method)
+//Create Recipe (POST Method)
 export const createRecipe = async (req, res) => {
   try {
     const newRecipe = new Recipes(req.body);
@@ -13,7 +13,7 @@ export const createRecipe = async (req, res) => {
   }
 };
 
-//Get All Recipes (Get Method)
+//Get All Recipes (GET Method)
 export const getAllRecipes = async (req, res) => {
   try {
     const getRecipes = await Recipes.find();
@@ -26,7 +26,7 @@ export const getAllRecipes = async (req, res) => {
   }
 };
 
-//Get A Single Recipe by Id (Get Method)
+//Get A Single Recipe by Id (GET Method)
 export const getRecipeById = async (req, res) => {
   try {
     const recipeId = req.params.id;
@@ -37,6 +37,27 @@ export const getRecipeById = async (req, res) => {
     res
       .status(200)
       .json({ message: "Recipe retrieved successfully", data: recipeDetails });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Update A Recipe by Id (PUT Method)
+export const updateRecipe = async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const { RecipeName, Ingredients, CookTime, Category } = req.body;
+    const result = await Recipes.findByIdAndUpdate(
+      { _id: recipeId },
+      { RecipeName, Ingredients, CookTime, Category },
+      { new: true }
+    );
+    if (!result) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Recipe updated successfully", data: result });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- Routers/recipeRouter.js
+++ Routers/recipeRouter.js
@@ -3,6 +3,7 @@ import {
   createRecipe,
   getAllRecipes,
   getRecipeById,
+  updateRecipe,
 } from "../Controllers/recipeController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/create", createRecipe);
 router.get("/getdata", getAllRecipes);
 router.get("/getdata/:id", getRecipeById);
+router.put("/update/:id", updateRecipe);
 
 export default router;
