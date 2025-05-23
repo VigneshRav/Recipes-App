0eb46cf9bcaf0555fc0dd2e587839ac8a09a2234
VigneshRav <vignesh212000@gmail.com>
2025-05-23 12:35:09 +0530

    Get All Recipes & Get Recipe By Id Completed

--- Controllers/recipeController.js
+++ Controllers/recipeController.js
@@ -12,3 +12,32 @@ export const createRecipe = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//Get All Recipes (Get Method)
+export const getAllRecipes = async (req, res) => {
+  try {
+    const getRecipes = await Recipes.find();
+    res.status(200).json({
+      message: "All Recipes retrieved successfully",
+      data: getRecipes,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Get A Single Recipe by Id (Get Method)
+export const getRecipeById = async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const recipeDetails = await Recipes.findById(recipeId);
+    if (!recipeDetails) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Recipe retrieved successfully", data: recipeDetails });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Routers/recipeRouter.js
+++ Routers/recipeRouter.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createRecipe } from "../Controllers/recipeController.js";
+import {
+  createRecipe,
+  getAllRecipes,
+  getRecipeById,
+} from "../Controllers/recipeController.js";
 
 const router = express.Router();
 
 router.post("/create", createRecipe);
+router.get("/getdata", getAllRecipes);
+router.get("/getdata/:id", getRecipeById);
 
 export default router;
